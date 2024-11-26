7401b8270aad174cedb63eb17f1a407c4b419403
Laurie <laurieswrInf@gmail.com>
2024-11-26 15:21:20 +0100

    new repository

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Accueil from './accueil/accueil.jsx';
-import Cadeaux from './cadeaux/cadeaux.jsx';
-import FilmsNoel from './filmNoel/film-noel.jsx';
-import Footer from './footer/footer.jsx';
-import { AuthProvider, AuthContext } from './header/AuthContext.jsx';
+import Accueil from './accueil/accueil';
+import Cadeaux from './cadeaux/cadeaux';
+import FilmsNoel from './filmNoel/film-noel';
+import Footer from './footer/footer';
+import { AuthProvider, AuthContext } from './header/AuthContext';
 import Navbar from './navbar/navbar';
 import Menu from './menu/menu';
-import Header from './header/header.jsx';
-import CompteRebours from './compte-rebours/compte-rebours.jsx';
-import ChristmasMusic from '../christmas-music/music';
+import Header from './header/header';
+import CompteRebours from './compte-rebours/compte-rebours';
+import ChristmasMusic from './christmas-music/music';
 
 function App() {
   const { user } = useContext(AuthContext);
